fbfbd02aab0e5842b1855e81c5282d1e0ec18538
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-02 07:05:59 -0700

    Framework: Add Canvas to Contain All Rendering
    This commit adds a canvas to handle all painting. :^)

--- scripts/framework.js
+++ scripts/framework.js
@@ -1,3 +1,19 @@
 window.NeonWidget = class {
     
 }
+window.neonContainer = document.createElement("canvas");
+neonContainer.style.position = "absolute";
+neonContainer.style.left = 0;
+neonContainer.style.top = 0;
+neonContainer.style.width = "100vw";
+neonContainer.style.height = "100vh";
+neonContainer.style.zIndex = 99999999;
+neonContainer.paint = () => {
+    neonContainer.width = innerWidth;
+    neonContainer.height = innerHeight;
+    window.neonPainter = neonContainer.getContext("2d");
+    neonPainter.fillStyle = "black";
+    neonPainter.fillRect(0,0,50,50);
+}
+setInterval(neonContainer.paint);
+document.body.appendChild(neonContainer);
