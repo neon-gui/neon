24c8d0eb16d1feac528b730de7b25ac9c6d386d4
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-02 16:49:42 -0700

    Core: Add Cursor

--- scripts/framework.js
+++ scripts/framework.js
@@ -9,19 +9,26 @@ window.BackgroundWidget = class extends NeonWidget {
 }
 window.neonContainer = document.createElement("canvas");
 window.bgWidget = new BackgroundWidget();
+window.latestMouseEvent = null;
 neonContainer.style.position = "absolute";
 neonContainer.style.left = 0;
 neonContainer.style.top = 0;
 neonContainer.style.width = "100vw";
 neonContainer.style.height = "100vh";
 neonContainer.style.zIndex = 99999999;
+neonContainer.style.cursor = "none";
 neonContainer.paint = () => {
     neonContainer.width = innerWidth;
     neonContainer.height = innerHeight;
     window.neonPainter = neonContainer.getContext("2d");
     bgWidget.render(neonPainter);
     neonPainter.fillStyle = "black";
-    neonPainter.fillRect(0,0,50,50);
+    if (!!e) {
+        neonPainter.fillRect(e.clientX,e.clientY,30,30);
+    }
+}
+neonContainer.onmousemove = (e) => {
+    latestMouseEvent = e;
 }
 setInterval(neonContainer.paint);
 document.body.appendChild(neonContainer);
