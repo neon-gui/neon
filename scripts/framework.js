44ae55c9ce4898ad4b8fd483bab599f280c8360e
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-02 16:21:05 -0700

    Core: Add a background
    This commit adds a red background.

--- scripts/framework.js
+++ scripts/framework.js
@@ -1,7 +1,14 @@
 window.NeonWidget = class {
     
 }
+window.BackgroundWidget = class extends NeonWidget {
+    render(painter) {
+        painter.fillStyle = "red";
+        painter.fillRect(0,0,painter.width,painter.height);
+    }
+}
 window.neonContainer = document.createElement("canvas");
+window.bgWidget = new BackgroundWidget();
 neonContainer.style.position = "absolute";
 neonContainer.style.left = 0;
 neonContainer.style.top = 0;
@@ -12,6 +19,7 @@ neonContainer.paint = () => {
     neonContainer.width = innerWidth;
     neonContainer.height = innerHeight;
     window.neonPainter = neonContainer.getContext("2d");
+    bgWidget.render(neonPainter);
     neonPainter.fillStyle = "black";
     neonPainter.fillRect(0,0,50,50);
 }
