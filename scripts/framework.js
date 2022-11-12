cfa26d17ba0a81faf1abb61de00a39ca5da813d4
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-12 07:30:51 -0800

    Framework: Fix options.isBeingDragged

--- scripts/framework.js
+++ scripts/framework.js
@@ -6,7 +6,7 @@ window.NeonWidget = class {
     render(painter, options) {
         var optionsClone = structuredClone(options);
         optionsClone.isBeingHovered = currentlyHoveredNode == this;
-        optionsClone.isBeingDragged = currentlyDraggedNode == this;
+        optionsClone.isBeingDragged = currentlyDraggedNode == this && neonMouseDown;
         optionsClone.addEventAbsorber = (absorber) => {
             neonAbsorbers.push(absorber);
         }
