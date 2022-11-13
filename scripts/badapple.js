226236b34fc25c2d5597e385f7b4b4c25fd6fdc9
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-13 12:17:28 -0800

    Bad Apple: Fix more of me being silly

--- scripts/badapple.js
+++ scripts/badapple.js
@@ -9,10 +9,10 @@ window.BadAppleWidget = class extends NeonWidget {
             this.playButton.text = "Play";
             var badAppleUrl = "https://guidipolito.github.io/Bad_apple_JS/Badapplesmall.mp4";
             this.badApple = document.createElement("video");
-            badApple.src = badAppleUrl;
+            this.badApple.src = badAppleUrl;
             this.badAppleRenderer = document.createElement("canvas");
-            badAppleRenderer.width = 150;
-            badAppleRenderer.height = 112;
+            this.badAppleRenderer.width = 150;
+            this.badAppleRenderer.height = 112;
             this.badAppleRendererContext = badAppleRenderer.getContext('2d');
         }
 
