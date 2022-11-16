624605a5d01655a6f445ddaf7324547716435121
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-15 18:50:44 -0800

    Switch: Change Background Color

--- scripts/switch.js
+++ scripts/switch.js
@@ -27,7 +27,7 @@ window.NeonSwitchWidget = class extends NeonWidget {
             transform:painter.getTransform(),
             node:this
         });
-        painter.fillStyle = `rgb(32,32,32)`;
+        painter.fillStyle = `rgb(128,128,128)`;
         painter.beginPath();
         painter.roundRect(options.x, options.y + (-height + options.width) / 2, options.width, height,height);
         painter.fill();
