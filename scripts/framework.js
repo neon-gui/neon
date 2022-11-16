a182db27d1c5559e5a8101be816f633b54e57263
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-15 18:04:35 -0800

    Framework: Quick Demo :^)

--- scripts/framework.js
+++ scripts/framework.js
@@ -112,12 +112,13 @@ xpkg.onloads.push(() => {
         neonContainer.height = innerHeight;
         window.neonPainter = neonContainer.getContext("2d");
         neonPainter.resetTransform();
-        /*badApple.render(neonPainter, {
+        badApple.render(neonPainter, {
             x: 0,
             y: 0,
             width: neonPainter.canvas.width,
             height: neonPainter.canvas.height
-        });*/
+        });
+        /*
         bgWidget.render(neonPainter, {
             x: 0,
             y: 0,
@@ -146,6 +147,7 @@ xpkg.onloads.push(() => {
             width: 50,
             height: 50
         });
+        */
         neonPainter.fillStyle = "black";
         var cursor = "https://codelikecraze.github.io/neon/cursors/pointer.png";
         neonPainter.resetTransform();
