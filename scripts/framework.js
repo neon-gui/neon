606dc35f56917a0b85068220766bbc3c5af4809f
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-11-26 14:26:27 -0800

    Framework: Fixeth 2

--- scripts/framework.js
+++ scripts/framework.js
@@ -184,12 +184,12 @@ xpkg.onloads.push(() => {
     DOMRect.prototype.includes = function (point) {
         return point.x > this.left && point.x < this.right && point.y > this.top && point.y < this.bottom;
     };
+    neonPainter = neonContainer.getContext("2d");
     neonPainter.save();
     neonContainer.paint = () => {
         neonAbsorbers = [];
         neonContainer.width = innerWidth;
         neonContainer.height = innerHeight;
-        neonPainter = neonContainer.getContext("2d");
         var primaryColor = new NeonColor(255, 64, 64); // blue is 64 128 255
         neonPainter.resetTransform();
         /*badApple.render(neonPainter, {
